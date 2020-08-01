354a95217b894429e669ca3b578c768c991e1d8d
Moisés Mariano <moisesduartem@gmail.com>
2020-08-01 10:01:30 -0300

    feat: logica da paginacao

--- huntweb/src/pages/main/index.js
+++ huntweb/src/pages/main/index.js
@@ -10,6 +10,8 @@ export default class Main extends Component {
      */
     state = {
         products: [],
+        productInfo: {},
+        page: 1,
     }
 
     /**
@@ -23,17 +25,41 @@ export default class Main extends Component {
      * Funções customizadas DEVEM ser em formato de arrow function, para evitar
      * erros de sobrescrita com o "this".
      */
-    loadProducts = async() => {
-        const response = await api.get('/products');
+    loadProducts = async(page = 1) => {
+        const response = await api.get(`/products?page=${page}`);
         /**
          * Setando os produtos de acordo com o estado.
          */
-        this.setState({ products: response.data.docs });
+
+        const { docs, ...productInfo } = response.data;
+
+        this.setState({ products: docs, productInfo, page });
     };
+
+    prevPage = () => {
+        const { page, productInfo } = this.state;
+
+        console.log(page)
+        
+        if (page === 1) return;
+
+        const pageNumber = page - 1;
+        this.loadProducts(pageNumber);
+    }
+
+    nextPage = () => {
+
+        const { page, productInfo } = this.state;
+
+        if (page === productInfo.pages ) return;
+
+        const pageNumber = page + 1;
+        this.loadProducts(pageNumber);
+    }
 
     render() {
 
-        const { products } = this.state;
+        const { products, page, productInfo } = this.state;
 
         return( <div className="product-list">
             {products.map(product => (
@@ -44,6 +70,12 @@ export default class Main extends Component {
                 </article>
 
         ))}
+
+            <div className="actions">
+                <button disabled={page === 1} onClick={this.prevPage}>Anterior</button>
+                <button disabled={page === productInfo.pages} onClick={this.nextPage}>Próximo</button>
+            </div>
+            
         </div>)
     }
 }
