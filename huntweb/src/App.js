bbb9501e79c4046c963b680b594f92b41bee3170
Moisés Mariano <moisesduartem@gmail.com>
2020-07-31 23:43:30 -0300

    feat: importando component Header

--- huntweb/src/App.js
+++ huntweb/src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
+import Header from './components/Header/index.js';
 
-class App extends Component {
-  render() {
-    return <div className="App">
-      <h1>Hello World!</h1>
-    </div>
-  }
-}
+const App = () => (
+  <div className="App">
+    <Header />
+  </div>
+);
+
 
 export default App;
